04ae6458f696fc7d7999dbc5943fb6b909e3d283
nixkleber <s0569066@htw-berlin.de>
2019-04-17 12:19:06 +0200

    Add files via upload

---
+++ runmerunner/src/test/java/de/htw/ai/kbe/runmerunner/AnnotationFinderTest.java
@@ -0,0 +1,23 @@
+package de.htw.ai.kbe.runmerunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AnnotationFinderTest
+{
+    @RunMe()
+    public void testGetRunMeMethods()
+    {
+    	
+    }
+    
+    public void testGetOtherMethods()
+    {
+    	
+    }
+}
--- runmerunner/src/test/java/de/htw/ai/kbe/runmerunner/AppTest.java
+++ runmerunner/src/test/java/de/htw/ai/kbe/runmerunner/AppTest.java
@@ -1,43 +1,49 @@
 package de.htw.ai.kbe.runmerunner;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+	Class testClass;
+	AnnotationFinderTest afTest;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
+	/**
      * Rigourous Test :-)
      */
     public void testApp()
     {
         assertTrue( true );
     }
-    
-    public void testGetRunMe()
+
+    @Test()
+    public void testGetRunMeMethods()
     {
-    	assertNotNull("TESTCASE_RUN_ME", AppTest.class);
+    	afTest = new AnnotationFinderTest();
+    	
+    	AnnotationFinder af = new AnnotationFinder(afTest.getClass());
+    	assertTrue(!af.getRunMeMethods().isEmpty());
     }
+    
+//    @Test()
+//    public void testGetOtherMethods()
+//    {
+//    	afTest = new AnnotationFinderTest();
+//    	
+//    	AnnotationFinder af = new AnnotationFinder(afTest.getClass());
+//    	assertTrue(!af.getOtherMethods().isEmpty());
+//    }
+    
+//    @Test()
+//    public void testNonInvocableMethods()
+//    {
+//    	
+//    }
+    
 }
